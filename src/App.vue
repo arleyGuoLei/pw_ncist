2bc8d2f6711144353616682a5767204b4d8cdb16
arley <34805850@qq.com>
2018-11-24 15:02:41 +0800

    稍微适配一下手机端

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 
   <el-container id="app">
-    <el-aside :width="menuWidth">
+    <el-aside class="pc-aside" :width="menuWidth">
       <el-menu :router="true" default-active="ks" class="el-menu" :collapse="isCollapse">
         <div class="collapse" @click="showMenu">
           <i class="el-icon-d-arrow-left" v-if="!isCollapse"></i>
@@ -37,7 +37,6 @@
           </el-menu-item-group>
 
 
-
         </el-submenu>
 
         <el-menu-item index="2" disabled>
@@ -50,6 +49,51 @@
 
     <el-container>
       <el-header id="header"><h2>加密算法演示系统</h2></el-header>
+      <el-aside :class="['phone-aside',{'phone-aside-height':isCollapse}]" :width="menuWidth">
+        <el-menu :router="true" default-active="ks" class="el-menu" :collapse="isCollapse">
+          <div class="collapse" @click="showMenu">
+            <i class="el-icon-d-arrow-left" v-if="!isCollapse"></i>
+            <i class="el-icon-d-arrow-right" v-else></i>
+          </div>
+          <el-submenu index="password">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span slot="title">密码学</span>
+            </template>
+            <el-menu-item-group title="古典密码实验">
+              <el-menu-item index="ks">凯撒加密</el-menu-item>
+              <el-menu-item index="wjne">维吉耐尔</el-menu-item>
+              <el-menu-item index="vernam">vernam</el-menu-item>
+              <el-menu-item index="rf">栅栏置换</el-menu-item>
+              <el-menu-item index="dlzh">单轮分栏</el-menu-item>
+            </el-menu-item-group>
+
+            <el-menu-item-group title="hash算法实验">
+              <el-menu-item index="md5">MD5</el-menu-item>
+              <el-menu-item index="sha">SHA</el-menu-item>
+            </el-menu-item-group>
+
+            <el-menu-item-group title="分组密码实验">
+              <el-menu-item index="des">DES</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="非对称密码实验">
+              <el-menu-item index="rsa">RSA</el-menu-item>
+            </el-menu-item-group>
+
+            <el-menu-item-group title="对称密码实验">
+              <el-menu-item index="aes">AES</el-menu-item>
+            </el-menu-item-group>
+
+
+          </el-submenu>
+
+          <el-menu-item index="2" disabled>
+            <i class="el-icon-document"></i>
+            <span slot="title">OTHER</span>
+          </el-menu-item>
+
+        </el-menu>
+      </el-aside>
       <el-container>
         <el-main>
           <router-view/>
@@ -62,6 +106,30 @@
 </template>
 
 <style>
+
+  @media screen and (min-width:426px){
+    .phone-aside{
+      display: none;
+    }
+  }
+
+  @media screen and (max-width:425px){
+    .phone-aside {
+      position: absolute;
+      z-index: 9;
+    }
+
+    .phone-aside-height {
+      height: 52px;
+      width: 48px!important;
+      overflow: hidden!important;
+    }
+    .pc-aside {
+      display: none;
+    }
+  }
+
+
   #app {
     position: absolute;
     top: 0;
